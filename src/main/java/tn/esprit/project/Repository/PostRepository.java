e5601c42f8da690cb169cd79eb26eb6f5da16804
bessem azizi <azizi.bessem@esprit.tn>
2022-02-25 23:38:56 -1100

    entity erreur

--- src/main/java/tn/esprit/project/Repository/PostRepository.java
+++ src/main/java/tn/esprit/project/Repository/PostRepository.java
@@ -1,8 +1,9 @@
 package tn.esprit.project.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import tn.esprit.project.Entities.Post;
 
-public interface PostRepository extends CrudRepository<Post , Long> {
+public interface PostRepository extends JpaRepository<Post,Long> {
 }
